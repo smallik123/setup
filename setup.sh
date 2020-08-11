e7f09b1494d75234a1a968ab219fc06713e2fc94
Sanjeev Mallik <smallik123@users.noreply.github.com>
2020-08-11 16:22:38 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 apt-get update update;
-sudo apt-get update
-sudo apt-get install vim -y
-sudo apt-get install apt-utils -y
-sudo apt-get update
-sudo apt-get install apache2 -y
-sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:ondrej/php
-sudo apt-get update
+sudo apt-get update;
+sudo apt-get install vim -y;
+sudo apt-get install apt-utils -y;
+sudo apt-get update;
+sudo apt-get install apache2 -y;
+sudo apt-get install software-properties-common -y;
+sudo add-apt-repository ppa:ondrej/php;
+sudo apt-get update;
 sudo apt-get install php7.1 -y \
   libapache2-mod-php7.1 php7.1-mcrypt \
   php7.1-cli php7.1-xml php7.1-zip php7.1-mysql php7.1-gd \
@@ -15,7 +15,7 @@ sudo apt-get install php7.1 -y \
   php7.1-gmp php7.1-bcmath php7.1-curl \
   php7.1-mbstring php7.1-imap \
   php7.1-sybase freetds-common freetds-bin \
-  php7.1-xdebug
+  php7.1-xdebug;
 
 sudo apt update;
 sudo apt install mysql-server;
